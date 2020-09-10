da167971dd7bc78dbcb7957cf721277d9ea0711b
yiv <yuefengshao@126.com>
2020-09-10 21:05:05 +0800

    update

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,68 +1,50 @@
+
+
+import 'package:english_words/english_words.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main()=>runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter 演示',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: '首页'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      title: "App",
+      home: Scaffold(
+        appBar: AppBar(title: Text("Bar"),),
+        body: Center(
+          child: RandomWords(),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggests = [];
+  final _fontStyle = TextStyle(fontSize: 18.0);
+  @override
+  Widget build(BuildContext context) {
+    return _buildSuggests();
+  }
+  Widget _buildSuggests(){
+    return ListView.builder(itemBuilder:(ctx, i){
+      if (i.isOdd) return Divider();
+      final _index = i ~/ 2;
+      if (_index >= _suggests.length){
+        _suggests.addAll(generateWordPairs().take(10));
+      }
+      return _buildRow(_suggests[_index]);
+    }
+    );
+  }
+  Widget _buildRow(WordPair pair){
+    return ListTile(title: Text(pair.asCamelCase, style: _fontStyle,),);
   }
 }
---
+++ page_router_transition/android/app/src/main/kotlin/com/example/page_router_transition/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.page_router_transition
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
